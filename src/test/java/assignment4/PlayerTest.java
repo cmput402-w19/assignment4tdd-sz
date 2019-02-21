401344ec2b2682f255a676786d450458651363c4
Zhimao Lin <zhimao@ualberta.ca>
2019-02-21 14:31:30 -0700

    [RED] Add TestCompareTo() and TestToString()

--- src/test/java/assignment4/PlayerTest.java
+++ src/test/java/assignment4/PlayerTest.java
@@ -2,6 +2,10 @@ package assignment4;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.assertTrue;
 
 public class PlayerTest {
@@ -32,5 +36,42 @@ public class PlayerTest {
         assertTrue(player.getPlayTime() == 0);
         player.setPlayTime(playTime);
         assertTrue(player.getPlayTime() == playTime);
+    }
+
+    @Test
+    public void TestCompareTo()
+    {
+        Player player1 = new Player("TestPlayer1");
+        Player player2 = new Player("TestPlayer2");
+        Player player3 = new Player("TestPlayer3");
+
+        player1.setPlayTime(100000);
+        player2.setPlayTime(200000);
+        player3.setPlayTime(300000);
+
+        List<Player> playerList = new ArrayList<Player>();
+        playerList.add(player3);
+        playerList.add(player2);
+        playerList.add(player1);
+
+        Collections.sort(playerList);
+
+        assertTrue(playerList.get(0).getPlayerName().equals(player1.getPlayerName()));
+        assertTrue(playerList.get(1).getPlayerName().equals(player2.getPlayerName()));
+        assertTrue(playerList.get(2).getPlayerName().equals(player3.getPlayerName()));
+    }
+
+    @Test
+    public void TestToString()
+    {
+        Player player = new Player(playerName);
+        long expectedPlayTimeInSec = 0;
+        String expectedResult = playerName + "\t\t" + "[" + expectedPlayTimeInSec + "]";
+        assertTrue(player.toString().equals(expectedResult));
+
+        player.setPlayTime(playTime);
+        expectedPlayTimeInSec = playTime / 1000;
+        expectedResult = playerName + "\t\t" + "[" + expectedPlayTimeInSec + "]";
+        assertTrue(player.toString().equals(expectedResult));
     }
 }
