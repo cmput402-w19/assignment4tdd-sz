0f5428badaf07361f3753a0af9727ede771a89b7
Zhimao Lin <zhimao@ualberta.ca>
2019-02-22 22:32:21 -0700

    [GREEN] Implement App class

--- src/main/java/assignment4/App.java
+++ src/main/java/assignment4/App.java
@@ -1,13 +1,34 @@
 package assignment4;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main()
     {
-//        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        String playAgain = "y";
+
+        Game game = new Game();
+
+        while (playAgain.equals("y")) {
+            System.out.println("Please enter player name:");
+            String playerName = scanner.nextLine();
+
+            game.player = new Player(playerName);
+            game.board.clear();
+
+            game.play();
+
+            game.scoreBoard.addPlayer(game.player);
+            game.scoreBoard.printScoreBoard();
+
+            System.out.println("Do you want to play again? (y/n)");
+            playAgain = scanner.nextLine().toLowerCase();
+        }
     }
 }
